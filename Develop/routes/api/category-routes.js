e2b63205338db40c88c1f4e3538f253899bc5c44
henrythedrip <146283648+henrythedrip@users.noreply.github.com>
2024-03-12 18:31:18 -0600

    created the routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -6,23 +6,95 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    include: [
+      {
+        model: Product,
+        attributes: ['product_name'],
+      },
+    ],
+  })
+  .then((category) => res.status(200).json(category))
+  .catch((err) => {
+       console.log(err);
+       res.status(400).json(err);
+     });
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: { id: req.params.id },
+    include: [
+      {
+        model: Product,
+        attributes: ['product_name'],
+      }
+    ],
+  })
+ .then((category) => res.status(200).json(category))
+ .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  // post body should look like this...
+  // {
+  //   "category_name": "Stuff"
+  // }
+  Category.create(req.body)
+ .then((category) => {
+    // TODO: here, we need to find all the things associated with the new category from the request body and then update them or something
+  
+    res.status(200).json(category)
+  })
+ .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
 });
 
+// TODO: understand why the product update changes the tags associated with the product and replaces it. we might need to do something similar with this model because categories have many products?
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  // post body should look like this...
+  // 	{
+	// 	"id": 6,
+	// 	"category_name": "Stuff",
+	// 	"products": []
+	// }
+  Category.update(req.body, {
+    where: {
+      id: req.params.id,
+    },  
+  })
+  .then((category) => {
+    // TODO: here, we need to update any fields about the category that were changed
+  
+    res.status(200).json(category)
+  })
+  .catch((err) => {
+         console.log(err);
+         res.status(400).json(err);
+  });
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+  .then((category) => res.status(200).json(category))
+  .catch((err) => {
+         console.log(err);
+         res.status(400).json(err);
+  });
 });
 
 module.exports = router;
--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -7,12 +7,47 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [
+      {
+        model: Category,
+        attributes: ['category_name'],
+      },
+      {
+        model: Tag,
+        attributes: ['tag_name'],
+      },
+    ],
+  })
+ .then((products) => res.status(200).json(products))
+ .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: { id: req.params.id },
+    include: [
+      {
+        model: Category,
+        attributes: ['category_name'],
+      },
+      {
+        model: Tag,
+        attributes: ['tag_name'],
+      },
+    ],
+  })
+ .then((products) => res.status(200).json(products))
+ .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
 });
 
 // create new product
@@ -40,6 +75,8 @@ router.post('/', (req, res) => {
       // if no product tags, just respond
       res.status(200).json(product);
     })
+
+
     .then((productTagIds) => res.status(200).json(productTagIds))
     .catch((err) => {
       console.log(err);
@@ -94,6 +131,16 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+  .then((product) => res.status(200).json(product))
+  .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
 });
 
 module.exports = router;
